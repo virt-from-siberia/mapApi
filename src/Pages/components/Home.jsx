1bf79787670314d1f6110d9bef1f9b4967d2fe90
Aleksey <virtyoz777@gmail.com>
2020-05-29 20:09:31 +0900

    init

---
+++ src/Pages/components/Home.jsx
@@ -0,0 +1,20 @@
+//external
+import React from "react";
+
+//internal
+import NavBar from "@/components/NavBar";
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <div className='home'>
+                <div className='container'>
+                    <h1>Home</h1>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,11 @@
+//external
+import React from "react";
+//internal
+
+// const items = <div className='navbar ui top fixed menu'></div>;
+
+const NavBar = () => {
+    return <div className='ui top fixed menu navbar '>nav</div>;
+};
+
+export default NavBar;
