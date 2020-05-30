8b2e945f44703556cfc72403c57048c6aa62e35d
Aleksey <virtyoz777@gmail.com>
2020-05-30 17:41:26 +0900

    header

--- src/Pages/components/Home.jsx
+++ src/Pages/components/Home.jsx
@@ -3,249 +3,41 @@ import React from "react";
 
 //internal
 import NavBar from "@/components/NavBar";
+import iconEarch from "@/img/logos/logo1.png";
+// import videoBg from "@/img/video/1/video.mp4";
 
 const Home = () => {
-  return (
-    <>
-      <div className="home">
-        <NavBar />
-        <div className="container">
-          <h1>Home</h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-          <h1>
-            Home ddasfsdf dsd fsd fsd fsd f sdf dsfsdfsdfsdfsdfsdf dsfsdfsdfsdf
-          </h1>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='home'>
+                <NavBar />
+                <div className='home__header'>
+                    {/* <video autoplay='true' playsinline>
+                        <source src={videoBg} type='video/mp4' />
+                    </video> */}
+                    <div className='home__header-about '>
+                        <div className='company-logo'>
+                            <img src={iconEarch} alt='logo' />
+                            <h1>
+                                <span>ГЕО</span> <br />
+                                СИСТЕМА
+                            </h1>
+                        </div>
+                        <div className='company-text'>
+                            <h2>Цифровая пплатформа для фермеров</h2>
+                            <p>
+                                Мы предоставляем широкий спектр возможностей для
+                                вашего бизнеса
+                            </p>
+                            <button class='ui green button'>
+                                Подключится к сервису
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,8 @@
 //external
 import React, { useState } from "react";
-import classNames from 'classnames'
+import classNames from "classnames";
 //internal
-import iconEarch from '@/img/earth.png';
-
+import iconEarch from "@/img/logos/logo1.png";
 
 const NavBar = () => {
     const [mobileNav, setmobileNav] = useState(false);
@@ -11,34 +10,51 @@ const NavBar = () => {
 
     const handleChanheMenu = () => {
         setmobileNav(!mobileNav);
-    }
+    };
 
     return (
-
-        <div className="nav-wrapper">
-            <nav className="navbar">
-                <img src={iconEarch} alt="Company Logo" />
-                <div className={classNames("menu-toggle ", {
-                    "is-active": mobileNav,
-                })}
-                    id="mobile-menu" onClick={handleChanheMenu}>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
+        <div className='nav-wrapper'>
+            <nav className='navbar'>
+                <img src={iconEarch} alt='Company Logo' />
+                <div
+                    className={classNames("menu-toggle ", {
+                        "is-active": mobileNav,
+                    })}
+                    id='mobile-menu'
+                    onClick={handleChanheMenu}
+                >
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
                 </div>
-                <ul className={classNames("nav no-search", { "mobile-nav": mobileNav })}>
-                    <li className="nav-item"><a href="#">Home</a></li>
-                    <li className="nav-item"><a href="#">About</a></li>
-                    <li className="nav-item"><a href="#">Work</a></li>
-                    <li className="nav-item"><a href="#">Contact Us</a></li>
-                    <li className="nav-item button-reg"><a href="#">Регистрация </a></li>
-                    <li className="nav-item button-log"><a href="#">Вход </a></li>
+                <ul
+                    className={classNames("nav no-search", {
+                        "mobile-nav": mobileNav,
+                    })}
+                >
+                    <li className='nav-item'>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#'>About</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#'>Work</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#'>Contact Us</a>
+                    </li>
+                    <li className='nav-item button-reg'>
+                        <a href='#'>Регистрация </a>
+                    </li>
+                    <li className='nav-item button-log'>
+                        <a href='#'>Вход </a>
+                    </li>
 
                     {/* <input className="search-input" type="text" placeholder="Search.." /> */}
                 </ul>
             </nav>
         </div>
-
     );
 };
 
